96c4b46dfeccf8611c31592f7604a8d802d5424c
sebastoss <sfpozzi@gmail.com>
2023-04-06 01:02:01 -0300

    cambios autorizados

--- src/componentes/Acceder.js
+++ src/componentes/Acceder.js
@@ -1,5 +1,5 @@
 function AccesoAutorizado(props){
-    return <h1>Bienvenidos, {props.nombre} tienes acceso autorizado</h1>;
+    return <h1>Bienvenidos, {props.nombre} tienes acceso autorizado ahora mismo</h1>;
 }
 
 function AccesoNoAutorizado(props){
