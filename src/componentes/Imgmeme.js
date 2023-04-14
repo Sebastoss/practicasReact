faefea65d2b6737d4338f8d4ba1e046c7225b79e
sebastoss <sfpozzi@gmail.com>
2023-04-14 00:46:13 -0300

    Se quitó código ocioso y se mejoró el css del text

--- src/componentes/Imgmeme.js
+++ src/componentes/Imgmeme.js
@@ -8,10 +8,6 @@ const Imgmeme = () => {
 
     const [textomeme, setTextomeme] = useState("");
     const [imgMeme, setImgMeme] = useState("");
-    const [ancho, setAncho] = useState("0px");
-    const [alto, setAlto] = useState("0px");
-    
-
     
     const textmeme = (ev) =>{
       setTextomeme(ev.target.value); 
@@ -22,25 +18,6 @@ const Imgmeme = () => {
       setImgMeme(ev.target.value); 
       console.log(ev.target.value);
     }
-    
-    
-    const imgCargada = ()=>{
-            const imgAltoAncho = document.getElementById('imgAltoAncho');
-
-            let width = `${imgAltoAncho.width}px`;
-            let height = `${imgAltoAncho.height}px`;
-            setAncho(width);
-            setAlto(height);
-            
-            const figures = document.getElementById('exportar');
-            figures.style = `width: ${width}px !important; height: ${height}px !important`;
-      
-            console.log(figures.style);
-            console.log(`Alto: ${alto} x Ancho: ${ancho}`);
-
-      
-    }
-
 
 
     const descarga = (ev) => {
@@ -61,11 +38,11 @@ const Imgmeme = () => {
         <h1 className='mt-5 mb-3'>Edita tu propio meme</h1>
         
         <h2 className='mt-2 mb-3'>Escribi tu frase</h2>
-        <input onChange={textmeme} className="form-control w-25 m-auto" type="text" placeholder="Pone tu frase" name="meme" value={textomeme} />
+        <input onChange={textmeme} className="form-control w-75 m-auto" type="text" placeholder="Pone tu frase" name="meme" value={textomeme} />
 
         <h2 className='mt-2 mb-3'>Elegi la imagen para tu meme</h2>
 
-        <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-50 m-auto" aria-label="Default select example">
+        <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-75 m-auto" aria-label="Default select example">
             <option>Selecciona una imagen</option>
             <option value={6}>Futurama</option>
             <option value={7}>Bob Esponja</option>
@@ -75,9 +52,11 @@ const Imgmeme = () => {
 
         <div>
           
-        <figure id='exportar' className='mb-1 w-75 m-auto'>
-          <p className='parrafo_superior mt-5 w-75 position-absolute top-10'>{textomeme}</p>
-          <img onLoad={imgCargada} id='imgAltoAncho' src={`../memesImg/${imgMeme}.png`} alt="Meme" className='figure-img mt-3 d-block m-auto w-100' />
+        <figure id='exportar' className='mb-1 m-auto'>
+       
+        
+          <p className='parrafo_superior position-absolute top-10'>{textomeme}</p>
+          <img id='imgAltoAncho' src={`../memesImg/${imgMeme}.png`} alt="Meme" className='figure-img mt-3 d-block m-auto w-100' />
         </figure>
         </div>
 
